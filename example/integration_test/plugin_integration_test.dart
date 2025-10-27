83254f62835487862b3c2f28f2786dcb9dcde72b
Adem Celik <admyvzclk@gmail.com>
2025-10-27 14:35:26 +0300

    tuya repository added

--- example/integration_test/plugin_integration_test.dart
+++ example/integration_test/plugin_integration_test.dart
@@ -16,7 +16,7 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('getPlatformVersion test', (WidgetTester tester) async {
-    final FlutterTyaPlugin plugin = FlutterTyaPlugin();
+    final FlutterTyaPlugin plugin = FlutterTyaPlugin.instance;
     final String? version = await plugin.getPlatformVersion();
     // The version string depends on the host platform running the test, so
     // just assert that some non-empty string is returned.
